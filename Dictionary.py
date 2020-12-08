8c77ca8708de766968a58aa22220b29f6f25d60e
Frey <74185756+Fr3y@users.noreply.github.com>
2020-12-08 17:53:06 +0530

    Work in progress....

---
+++ Dictionary.py
@@ -0,0 +1,9 @@
+# here we goes again 
+
+# list - collection of vallues ( all the values mix) - mutable ----- []
+
+# tuple - collection too cant be change (immutable) ----- ()
+
+# set - collection again but dont have indexing ---- {}
+
+
