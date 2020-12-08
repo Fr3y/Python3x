671be2c87f93eb01123ad1da9998827aee4a543e
Frey <74185756+Fr3y@users.noreply.github.com>
2020-12-08 17:52:16 +0530

    Tuples

---
+++ Tuple.py
@@ -0,0 +1,29 @@
+#here we go to the tuples
+#it is almost the same and the difference between there is this it is immutable xD (well it's not like me i guess)
+
+frey = (12,43,75) #now the main difference is that you can't change it
+
+frey[1]
+
+#well what i see is tuple have too methods
+
+tuple.index(frey)
+tuple.count(frey)
+
+#but we can't change the values
+
+# NOw time for SET's 
+
+s = {4,12,64,73}
+# it will Never Follow the sequence 
+#wanna see some intresting thing Here
+
+s = {14,64,85,25,13,6}
+# well, when you run this you will got 13 for one time, (it works on hash)
+
+#change a value but it's not support indexing
+
+# try by yourself by using pyhton docs 
+
+s.add(1:24)
+
